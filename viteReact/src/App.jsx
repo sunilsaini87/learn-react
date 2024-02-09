b152022fe88e9eb9dd1dcd70d8b0d69dedad5f62
Sunil Saini <sunilkumarsaini6363@gmail.com>
2024-02-09 19:14:55 +0530

    Add files via upload

---
+++ viteReact/src/App.jsx
@@ -0,0 +1,12 @@
+import Tea from "./tea";
+
+function App() {
+  return (
+    <>
+      <Tea />
+      <h1>Hello</h1>
+      <p>this is is is </p>
+    </>
+  );
+}
+export default App;
---
+++ viteReact/src/tea.jsx
@@ -0,0 +1,6 @@
+function Tea(){
+    return (
+        <h3>Tea is ready</h3>
+    )
+}
+export default Tea
